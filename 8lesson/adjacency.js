7c3d36392cad161f24ff589fdf827853196a2d56
Nikolay Kotlyarov <nikolay.kotlyarov1@gmail.com>
2022-11-09 00:05:10 +0600

    feat: matrix

--- 8lesson/adjacency.js
+++ 8lesson/adjacency.js
@@ -1,29 +1,31 @@
 async function build() {
-    console.log("Hello world");
     const nodes = await d3.csv("nodelist.csv");
+    const projects = await d3.csv("projects.csv");
     const edges = await d3.csv("edgelist.csv");
 
-    // console.table(nodes);
-    // console.table(edges);
-    function adjacencyMatrix(nodes, edges) {
+    function adjacencyMatrix(nodes,projects, edges) {
         var matrix = [];
         var edgeHash = {};
         edges.forEach(edge => {
-            var id = edge.source+"-"+edge.target;
+            var id = edge.company+"-"+edge.project;
             edgeHash[id] = edge;
         })
+
+        console.log(nodes[1].id)
+
         for(let i=0; i<nodes.length; i++) {
-            for(let j=0; j<nodes.length; j++) {
+            for(let j=0; j<projects.length; j++) {
                 var uel = nodes[i];
-                var bel = nodes[j];
+                var bel = projects[j];
                 var grid = {
-                    id: uel.id+"-"+bel.id,
+                    id: uel.company+"-"+bel.project,
                     x:j,
                     y:i,
+                    project: bel.group,
                     weight:0
                 }
                 if(edgeHash[grid.id]) {
-                    grid.weight = edgeHash[grid.id].weight;
+                    grid.weight = parseInt(edgeHash[grid.id].number);
                 }
                 matrix.push(grid);
 
@@ -33,13 +35,13 @@ async function build() {
     }
 
     var dimension = {
-        width: window.innerWidth*0.8,
-        height: window.innerWidth*0.8,
+        width: window.innerWidth*0.9,
+        height: window.innerWidth*0.9,
         margin: {
-            top: 50,
+            top: 400,
             right: 10,
             bottom: 10,
-            left: 55
+            left: 350
         }
     }
 
@@ -58,8 +60,8 @@ async function build() {
 
     const bounds = wrapper.append("g")
         .style("transform",`translate(${dimension.margin.left}px,${dimension.margin.top}px)`);
-    var data = adjacencyMatrix(nodes,edges);
-    // console.log(adjacencyMatrix(nodes,edges));
+    var data = adjacencyMatrix(nodes,projects, edges);
+    console.table(data);
     const pole = bounds
         .selectAll("rect")
         .data(data)
@@ -70,29 +72,55 @@ async function build() {
         .attr("height",25)
         .attr("x", d=>d.x*25)
         .attr("y", d=>d.y*25)
-        .style("fill-opacity", d=>d.weight*0.2)
+        .style("fill", d=>d.project === "satellite" ? "green" : d.project === "spaceplane" ? "red" :
+            d.project === "joint venture" ? "yellow" : d.project === "plane" ? "purple" :
+                d.project === "telescope" ? "firebrick" : d.project === "ISS" ? "tomato" : d.project === "missile" ? "black" : "black")
+        .style("fill-opacity", d=>d.weight*0.3)
 
     const namesX = wrapper
         .append("g")
-        .attr("transform","translate(55,25)")
+        .attr("transform","translate(350,260)")
         .selectAll("text")
-        .data(nodes)
+        .data(projects)
         .enter()
         .append("text")
         .attr("y",(d,i)=> i*25+12.5)
-        .text(d=>d.id)
+        .text(projects=>projects.project)
+        .style("text-anchor","middle")
+        .attr("transform", "rotate(270)");
+
+    const namesX1 = wrapper
+        .append("g")
+        .attr("transform","translate(350,60)")
+        .selectAll("text")
+        .data(projects)
+        .enter()
+        .append("text")
+        .attr("y",(d,i)=> i*25+12.5)
+        .text(projects=>projects.group)
         .style("text-anchor","middle")
         .attr("transform", "rotate(270)");
 
     const namesY = wrapper
         .append("g")
-        .attr("transform","translate(45,50)")
+        .attr("transform","translate(340,400)")
         .selectAll("text")
         .data(nodes)
         .enter()
         .append("text")
         .attr("y",(d,i)=> i*25+12.5)
-        .text(d=>d.id)
+        .text(d=>d.company)
+        .style("text-anchor","end");
+
+    const namesY1 = wrapper
+        .append("g")
+        .attr("transform","translate(75,400)")
+        .selectAll("text")
+        .data(nodes)
+        .enter()
+        .append("text")
+        .attr("y",(d,i)=> i*25+12.5)
+        .text(d=>d.country)
         .style("text-anchor","end");
 
 }
