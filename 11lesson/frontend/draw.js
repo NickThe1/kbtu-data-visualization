51d0782b98c765b2c25e30b4c945e2727c44b136
Nikolay Kotlyarov <nikolay.kotlyarov1@gmail.com>
2022-12-02 18:55:06 +0600

    feat: finish

--- 11lesson/frontend/draw.js
+++ 11lesson/frontend/draw.js
@@ -1,3 +1,64 @@
+async function buildPlot() {
+    const data = await d3.json("umap.json");
+    const dateParser = d3.timeParse("%%d-%m-Y");
+
+    const groupedData = await d3.json("group.json");
+    const dateParserGR = d3.timeParse("%%m-Y");
+
+    const yAccessor = (d) => d.y;
+    const xAccessor = (d) => dateParser(d.date_publ);
+
+    const yGRAccessor = (d) => d.y;
+    const xGRAccessor = (d) => dateParser(d.date_publ);
+
+    var dimension = {
+        width: window.innerWidth*0.9,
+        height: 600,
+        margin: {
+            top: 15,
+            left: 15,
+            bottom: 15,
+            right: 15
+        }
+    };
+
+    dimension.boundedWidth = dimension.width - dimension.margin.left - dimension.margin.right;
+    dimension.boundedHeight = dimension.height - dimension.margin.top - dimension.margin.bottom;
+
+    const wrapper = d3.select("#wrapper");
+    const svg = wrapper.append("svg")
+    svg.attr("height",dimension.height);
+    svg.attr("width",dimension.width);
+    const bounded = svg.append("g");
+    bounded.style("transform",`translate(${dimension.margin.left}px, ${dimension.margin.top})`);
+
+    const yScaler = d3.scaleLinear()
+        .domain(d3.extent(data,yAccessor))
+        .range([dimension.boundedHeight,50]);
+    te = groupedData
+    //added scaler
+    const tempHighScaler = d3.scaleLinear()
+        .domain(d3.extent(data,tempHighAccessor))
+        .range([dimension.boundedHeight,50]);
+
+    const xScaler = d3.scaleTime()
+        .domain(d3.extent(data,xAccessor))
+        .range([0,dimension.boundedWidth]);
+    re = dateParserGR
+    var lineGenerator = d3.line()
+        .x(d => xScaler(xAccessor(d)))
+        .y(d => tempHighScaler(yAccessor(d)));
+
+
+    var xAxis = d3.axisBottom()
+        .scale(xScaler);
+
+    var yAxis = d3.axisLeft()
+        .scale(tempHighScaler);
+
+    yAxis.tickFormat( (d,i) => d + "F")
+}
+
 function getRandomData(ordinal = false) {
 
     const NGROUPS = 5,
