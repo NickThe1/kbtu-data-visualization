cd093994b2be6cd188e3f1a7dddcd8d221f2cf54
Nurlan <49631392+NurlanNurmash@users.noreply.github.com>
2022-12-09 01:46:05 +0600

    Update graph.js

--- probability-cone/python/app/static/graph.js
+++ probability-cone/python/app/static/graph.js
@@ -1,162 +1,81 @@
-async function drawBar(btnId, data, clear) {
+async function clear(){
+    const parent = d3.select("#wrapper")
+    parent.selectAll('*').remove()
 
-    if (clear){
-        var dots = [[]]
-        var dots1 = [[]]
-    }
-    else {
-        var dots = generateRandomDots(20);
-        var dots1 = generateRandomDots(20);
-    }
+    var width = 700;
+    var height = 500;
+    var spacing = 100;
+    var svg = parent.append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate("+ spacing/2 + "," + spacing/2 + ")")
 
-    const dataset = null
-    //Accessor
-    const xAccessor = d => d[data];
-    const yAccessor = d => d.length;
-    console.log(yAccessor(data))
-    // to shift x & y axes and data to right
-    const shift = 50;
-
-    const width = 800
-    let dimensions = {
-        width: width,
-        height: width * 0.6,
-        margin: {
-            top: 20,
-            right: 30,
-            bottom: 20,
-            left: 30,
-        },
-    }
-    dimensions.boundedWidth = dimensions.width
-        - dimensions.margin.left
-        - dimensions.margin.right
-    dimensions.boundedHeight = dimensions.height
-        - dimensions.margin.top
-        - dimensions.margin.bottom
-
-    // 3. Draw canvas
-
-    const wrapper = d3.select("#wrapper")
-        .html("") // clear div before drawing
-        .append("svg")
-        .attr("width", dimensions.width)
-        .attr("height", dimensions.height);
-
-    const bounds = wrapper.append("g")
-        .style("translate",`translate(${dimensions.margin.left}px,${dimensions.margin.top}px)`);
-
-    const xScaler = d3.scaleLinear()
+    var xScaler = d3.scaleLinear()
         .domain([0, 100])
-        .range([0,dimensions.boundedWidth])
-        .nice()
-
-    const yScaler = d3.scaleLinear()
+        .range([0, width-spacing-50]);
+    var yScaler = d3.scaleLinear()
         .domain([0, 100])
-        .range([dimensions.boundedHeight,0])
+        .range([height-spacing, 0])
+    var xAxis = d3.axisBottom(xScaler)
+    var yAxis = d3.axisLeft(yScaler)
 
-    const xAxisGen = d3.axisBottom()
-        .scale(xScaler);
-    const xAxis = bounds.append("g")
-        .call(xAxisGen)
-        .attr("transform", `translate(${shift},${dimensions.boundedHeight} )`);
+    svg.append("g")
+        .attr("transform", "translate(30, " + (height-spacing) + ")")
+        .call(xAxis)
+    svg.append("g")
+        .attr("transform", "translate(30,0)")
+        .call(yAxis)
 
-    // y axis
-    const yAxisGen = d3.axisLeft()
-        .scale(yScaler);
-    const yAxis = bounds.append("g")
-        .call(yAxisGen)
-        .attr("transform", `translate(${shift}, 0)`);
+    svg.append('text')
+        .attr("x", height/2)
+        .attr("y", height-60)
+        .text("Date")
+    svg.append('text')
+        .attr("y", height/2.5)
+        .attr("x", -35)
+        .text("Open")
+    svg.append('text')
+        .attr("y", -20)
+        .attr("x", width/3)
+        .style("font-size", "20px")
+        .text("Name")
 
-    const xLabel = bounds.append("text")
-        .attr("x",dimensions.boundedWidth - 30)
-        .attr("y",dimensions.boundedHeight + 30)
-        .text("X")
-        .attr("fill","black")
-        .attr("font-size","15px")
-        .attr("text-anchor","middle");
+    var svg2 = parent.append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate("+ spacing/2 + "," + spacing/2 + ")")
 
-    const yLabel = bounds.append("text")
-        .attr("x",20)
-        .attr("y",30)
-        .text("Y")
-        .attr("fill","black")
-        .attr("font-size","15px")
-        .attr("text-anchor","middle");
+    var xScaler = d3.scaleLinear()
+        .domain([0, 100])
+        .range([0, width-spacing-50]);
+    var yScaler = d3.scaleLinear()
+        .domain([0, 100])
+        .range([height-spacing, 0])
+    var xAxis = d3.axisBottom(xScaler)
+    var yAxis = d3.axisLeft(yScaler)
 
-    const blue = bounds.append("text")
-        .attr("x",100)
-        .attr("y",40)
-        .text("Blue rect")
-        .attr("fill","blue")
-        .attr("font-size","15px")
-        .attr("text-anchor","middle");
+    svg2.append("g")
+        .attr("transform", "translate(30, " + (height-spacing) + ")")
+        .call(xAxis)
+    svg2.append("g")
+        .attr("transform", "translate(30,0)")
+        .call(yAxis)
 
-    const red = bounds.append("text")
-        .attr("x",100)
-        .attr("y",20)
-        .text("Red dots")
-        .attr("fill","red")
-        .attr("font-size","15px")
-        .attr("text-anchor","middle");
-
-
-    const scatter = bounds.append('g')
-        .selectAll("dot")
-        .data(dots)
-        .enter()
-        .append("circle")
-        .attr("cx", function (d) { return xScaler(d[0]); } )
-        .attr("cy", function (d) { return yScaler(d[1]) + shift; } )
-        .attr("r", 4)
-        .attr("transform", "translate(" + 100 + "," + -shift + ")")
-        .style("fill", "red");
-
-    const scatter1 = bounds.append('g')
-        .selectAll("dot")
-        .data(dots1)
-        .enter()
-        .append("rect")
-        .attr("x", function (d) { return xScaler(d[0]); } )
-        .attr("y", function (d) { return yScaler(d[1]) + shift; } )
-        //.attr("r", 4)
-        .attr("width", 8)
-        .attr("height", 8)
-        .attr("transform", "translate(" + 100 + "," + -shift + ")")
-        .style("fill", "blue");
-
-    //coloring buttons
-    changeActiveButton(btnId);
+    svg2.append('text')
+        .attr("x", height/2)
+        .attr("y", height-60)
+        .text("Date")
+    svg2.append('text')
+        .attr("y", height/2.5)
+        .attr("x", -35)
+        .text("Open")
+    svg2.append('text')
+        .attr("y", -20)
+        .attr("x", width/3)
+        .style("font-size", "20px")
+        .text("Derivative")
 }
+clear()
 
-function generateRandomDots(amount){
-    var dots = []; // Initialize array
-    for (var i = 0 ; i < amount; i++) {
-        dots[i] = []; // Initialize inner array
-        for (var j = 0; j < 2; j++) {
-            if (i % 2 === 1){
-                dots[i][j] = Math.random() * 90 ;
-            }
-            else {
-                dots[i][j] = Math.random() * 90 ;
-            }
-
-        }
-    }
-    return dots;
-}
-
-function changeActiveButton(id){
-    if(id == null){
-        return 0;
-    }
-    //remove active class
-    const activeButton = document.getElementsByClassName("active");
-    activeButton[0].classList.remove("active");
-
-    //add active class to clicked button
-    const clickedButton = document.getElementById(id)
-    clickedButton.classList.add("active");
-}
-
-drawBar(null, "temperatureLow");
